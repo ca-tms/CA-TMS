5fdc917a104ea2ed447f71054d70c4d519104f58
Jannik Vieten <jannik.vieten@stud.tu-darmstadt.de>
2013-12-14 15:28:12 +0100

    added extension building blocks; ready to implement

---
+++ trustviewsextension@cdc.informatik.tu-darmstadt.de/content/browserOverlay.js
@@ -0,0 +1,15 @@
+/**
+ * Define Trust Views Extension (TVE) namespace.
+ */
+if("undefined" == typeof(TVE)) {
+    var TVE = {};
+};
+
+/**
+ * Controls the browser overlay.
+ */
+TVE.BrowserOverlay = {
+    
+    
+    
+};
