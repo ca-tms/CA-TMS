1149334c7723de94df52c1cc437e3341144bc071
Jannik Vieten <jannik.vieten@stud.tu-darmstadt.de>
2014-01-17 20:18:26 +0100

    added default preferences file

---
+++ trustviewsextension@cdc.informatik.tu-darmstadt.de/defaults/preferences/defaults.js
@@ -0,0 +1,3 @@
+pref("extensions.trustviewsextension.ctmsURL", "http://localhost");
+pref("extensions.trustviewsextension.ctmsPort", "8084");
+pref("extensions.trustviewsextension.secLevel", "medium");
