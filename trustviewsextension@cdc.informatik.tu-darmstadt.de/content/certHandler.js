238a1a3c407065264925c48b37541bbac397b1aa
Jannik Vieten <jannik.vieten@stud.tu-darmstadt.de>
2014-01-17 20:26:08 +0100

    reversed cert chain order

--- trustviewsextension@cdc.informatik.tu-darmstadt.de/content/certHandler.js
+++ trustviewsextension@cdc.informatik.tu-darmstadt.de/content/certHandler.js
@@ -10,10 +10,10 @@ TVE.CertHandler = {
         let serverCert = sslStatus.serverCert;
         
         // store certificate chain in array
-        let certChain = [];         // as JavaScript objects
-        let rawDERcertChain = [];   // as byte arrays, representing cert in DER format
-        // server's certificate is the first one, root CA's certificate is the last one
-        for(let i = 0; i < serverCert.getChain().length; i++) {
+        let certChain = new Array(serverCert.getChain().length);         // as JavaScript objects
+        let rawDERcertChain = new Array(serverCert.getChain().length);   // as byte arrays, representing cert in DER format
+        // server's certificate is the last one, root CA's certificate is the first one
+        for(let i = serverCert.getChain().length-1; i >= 0; i--) {
             certChain[i] = serverCert.getChain().queryElementAt(i, Components.interfaces.nsIX509Cert);
             rawDERcertChain[i] = certChain[i].getRawDER(new Object());
         }
