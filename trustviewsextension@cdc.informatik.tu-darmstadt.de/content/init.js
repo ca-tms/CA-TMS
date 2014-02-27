89c39a78fc175e29c2551d1c898f8810309bd148
Jannik Vieten <jannik.vieten@stud.tu-darmstadt.de>
2014-02-27 18:18:06 +0100

    on installation, setup task like button adding is done, now

--- trustviewsextension@cdc.informatik.tu-darmstadt.de/content/init.js
+++ trustviewsextension@cdc.informatik.tu-darmstadt.de/content/init.js
@@ -1,2 +1,21 @@
 // register SSL Listener
 getBrowser().addProgressListener(TVE.SSLListener);
+
+
+// on first run:
+// * add toolbarbutton automatically
+// * select default security level menuitem
+window.addEventListener("load", function() { TVE.installButton(); }, false);
+TVE.installButton = function() {
+    if(!TVE.Prefs.getBoolPref("firstRunDone")) {
+        TVE.Prefs.setBoolPref("firstRunDone", true);
+        
+        let toolbar = document.getElementById("nav-bar");
+        toolbar.insertItem("trustviewsextension-toolbarbutton", null);
+        toolbar.setAttribute("currentset", toolbar.currentSet);
+        document.persist(toolbar.id, "currentset");
+        
+        let item = document.getElementById("trustviewsextension-secLevelMenuitem-medium");
+        item.setAttribute("checked", true);
+    }
+};
--- trustviewsextension@cdc.informatik.tu-darmstadt.de/defaults/preferences/defaults.js
+++ trustviewsextension@cdc.informatik.tu-darmstadt.de/defaults/preferences/defaults.js
@@ -1,3 +1,4 @@
+pref("extensions.trustviewsextension.firstRunDone", false);
 pref("extensions.trustviewsextension.ctmsURL", "http://localhost");
 pref("extensions.trustviewsextension.ctmsPort", "8084");
 pref("extensions.trustviewsextension.secLevel", "medium");
