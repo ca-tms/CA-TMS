9b5695ab52c15c6baa50c650795c2610fe64fdf9
Pascal Weisenburger <pascal.weisenburger@web.de>
2014-03-03 18:50:21 +0100

    key legitimacy fixation implemented

--- trustviewapplication/src/data/Configuration.java
+++ trustviewapplication/src/data/Configuration.java
@@ -3,6 +3,7 @@ package data;
 public interface Configuration extends AutoCloseable {
 	static String OPINION_N = "opinion-n";
 	static String OPINION_MAX_F = "opinion-max-f";
+	static String FIX_KL = "fix-kl";
 	static String ASSESSMENT_EXPIRATION_MILLIS = "assessment-expiration-millis";
 
 	public <T> T get(String key, Class<T> type)
