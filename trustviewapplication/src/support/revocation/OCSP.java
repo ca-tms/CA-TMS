effa67013c4e0a87967ccd14cfb246a6426c7221
Pascal Weisenburger <pascal.weisenburger@web.de>
2014-03-26 00:15:12 +0100

    added comments to clarify that support.revocation is intentionally unimplemented

---
+++ trustviewapplication/src/support/revocation/OCSP.java
@@ -0,0 +1,3 @@
+// Standard path validation including all validity and revocation checking
+// is done by Firefox and passed on to this application via the Firefox plugin.
+// The application itself does not implement it currently.
