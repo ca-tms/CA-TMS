dde50c553615d55eac32ead06260956c74985ca5
Jannik Vieten <jannik.vieten@stud.tu-darmstadt.de>
2014-03-29 10:04:17 +0100

    fixed possible nullpointer

--- trustviewsextension@cdc.informatik.tu-darmstadt.de/content/sslListener.js
+++ trustviewsextension@cdc.informatik.tu-darmstadt.de/content/sslListener.js
@@ -10,65 +10,69 @@ TVE.SSLListener = {
      */
     onLocationChange: function(aBrowser, aWebProgress, aRequest, aLocation, aFlags) {
         
-        // suspend request as long as it's possible (does not work later in the code)
-        aRequest.suspend();
+        if(aRequest != null) {
         
-        let url = aLocation.asciiSpec;
-        
-        // if it's a HTTPS request and page is not excluded for this session:
-        if(aRequest != null && aLocation.scheme == "https" && !TVE.State.isAllowedPage(url)) {
+            // suspend request as long as it's possible (does not work later in the code)
+            aRequest.suspend();
             
-            var doValidation = {
-                notify: function(timer) {
-                    try {
-                        
-                        // parse standard validation result from Firefox/NSS
-                        let validationResult = TVE.CertHandler.getValidationResult(aBrowser);
-                        
-                        if(validationResult == "valid") {
-                        
-                            // gather data for upcoming CTMS validation
-                            let rawChain = TVE.CertHandler.getRawChain(aBrowser);
-                            let secLevel = TVE.Prefs.getCharPref("secLevel");
+            let url = aLocation.asciiSpec;
+            
+            // if it's a HTTPS request and page is not excluded for this session:
+            if(aLocation.scheme == "https" && !TVE.State.isAllowedPage(url)) {
+                
+                var doValidation = {
+                    notify: function(timer) {
+                        try {
                             
-                            try {
-                                // query CTMS!
-                                let ctmsResult = TVE.CTMSCommunicator.requestValidation(url, rawChain, validationResult, secLevel);
-                                if(ctmsResult == "UNTRUSTED") {
+                            // parse standard validation result from Firefox/NSS
+                            let validationResult = TVE.CertHandler.getValidationResult(aBrowser);
+                            
+                            if(validationResult == "valid") {
+                            
+                                // gather data for upcoming CTMS validation
+                                let rawChain = TVE.CertHandler.getRawChain(aBrowser);
+                                let secLevel = TVE.Prefs.getCharPref("secLevel");
+                                
+                                try {
+                                    // query CTMS!
+                                    let ctmsResult = TVE.CTMSCommunicator.requestValidation(url, rawChain, validationResult, secLevel);
+                                    if(ctmsResult == "UNTRUSTED") {
+                                        aRequest.resume();
+                                        // display warning page when result is bad
+                                        TVE.State.untrusted(aBrowser, url);
+                                    } else {
+                                        aRequest.resume();
+                                    }
+                                } catch(err) {
+                                    // happens when CTMS server is unreachable
                                     aRequest.resume();
-                                    // display warning page when result is bad
-                                    TVE.State.untrusted(aBrowser, url);
-                                } else {
-                                    aRequest.resume();
+                                    TVE.State.unreachable(aBrowser, url);
                                 }
-                            } catch(err) {
-                                // happens when CTMS server is unreachable
+                            
+                            } else {
+                                // when standard validation result is not valid
                                 aRequest.resume();
-                                TVE.State.unreachable(aBrowser, url);
                             }
-                        
-                        } else {
-                            // when standard validation result is not valid
-                            aRequest.resume();
+                            
+                        }
+                        catch(err) {
+                            // maybe it's too early to access the SSLStatus when the event handler kicks in
+                            // in this case CertHandler.getValidationResult() throws an error which is caught here
+                            // wait 10 ms and try it again
+                            var timer = Components.classes["@mozilla.org/timer;1"].createInstance(Components.interfaces.nsITimer);
+                            timer.initWithCallback(doValidation, 10, Components.interfaces.nsITimer.TYPE_ONE_SHOT);
                         }
                         
                     }
-                    catch(err) {
-                        // maybe it's too early to access the SSLStatus when the event handler kicks in
-                        // in this case CertHandler.getValidationResult() throws an error which is caught here
-                        // wait 10 ms and try it again
-                        var timer = Components.classes["@mozilla.org/timer;1"].createInstance(Components.interfaces.nsITimer);
-                        timer.initWithCallback(doValidation, 10, Components.interfaces.nsITimer.TYPE_ONE_SHOT);
-                    }
-                    
                 }
+                
+                doValidation.notify();
+                
+            } else {
+                // continue normally when it's not a HTTPS request, we want to intercept
+                aRequest.resume();
             }
-            
-            doValidation.notify();
-            
-        } else {
-            // continue normally when it's not a HTTPS request, we want to intercept
-            aRequest.resume();
+        
         }
         
     },
@@ -79,6 +83,8 @@ TVE.SSLListener = {
 
     onSecurityChange: function(aBrowser, aWebProgress, aRequest, aState) {
         // nothing to do here
+        // the idea was to do the handling here, but unfortunately there is no onSecurityChange event fired
+        // when a link is opened in a new tab, so this becomes useless
     },
 
     onStateChange: function(aBrowser, aWebProgress, aRequest, aStateFlags, aStatus) {
